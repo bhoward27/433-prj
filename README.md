cdea55a1809e5fe5158072fdcbbeb167566986ff
Benjamin Howard <bentlife@icloud.com>
2023-03-16 18:31:56 -0700

    Integrate Twilio SMS demo code into main.cpp.
    Add installTwilioDependencies.sh script.

--- README.md
+++ README.md
@@ -7,9 +7,17 @@ host contains all files relevant to code which runs on the host (Debian PC/VM).
 Source code goes into the src folder within the target or host folder.
 
 Dependencies
+(It is assumed that the gcc cross-compiler and binutils have already been installed (as any students would have done for assignment 1).)
 You will need the g++ cross-compiler to build the code in target/src. To install it, run this command on your host:
 `sudo apt install g++-arm-linux-gnueabihf`
+You will also need to install some additional cross-compiled libraries that are required by Twilio SMS service: OpenSSL, zlib, and curl.
+These must be installed to /usr/arm-linux-gnueabihf on your host. Run the following command on your host to do so:
+`./installTwilioDependencies.sh`
+Note: when the above script is downloading source code, it may fail to connect several times. This is normal, just wait and eventually it should succeed.
+If the file `target/src/twilio/config.h` is not present, ask for the file from a team member.
+Regardless, please change the `toPhoneNumber` constant in that file to your own personal phone number (add +1 in front for Canadian numbers).
 
 Hardware Setup
 To get temperature readings, the TMP36 temperature sensor must be wired up to the BeagleBone as shown in this student guide (NOTE: this guide was not created by The Defenders): https://opencoursehub.cs.sfu.ca/bfraser/grav-cms/cmpt433/links/files/2022-student-howtos/TMP36TemperatureSensor.pdf
 For our project, the sensor must be connected to AIN1.
+# TODO: Update the above section for wiring up with voltage divider + adjust conversion formula in code (likely just multiply by 2).
--- target/src/main.cpp
+++ target/src/main.cpp
@@ -1,18 +1,113 @@
 #include <iostream>
+#include <string>
 
 #include "heat_sampler.h"
+#include "twilio/twilio.h"
+#include "twilio/config.h"
 
 int main() {
     std::cout << "Hello BeagleBone!\n";
 
     ShutdownManager shutdownManager;
 
-    // Sample the temperature at 1 Hz and calculate window average over the last 10 samples.
-    HeatSampler heatSampler(&shutdownManager, 1, 10, true);
-    // Construct like this instead if you want the sampler to not print anything:
-    // HeatSampler heatSampler(&shutdownManager, 1, 10);
+    // TODO: Add back in after wired up.
+    // // Sample the temperature at 1 Hz and calculate window average over the last 10 samples.
+    // HeatSampler heatSampler(&shutdownManager, 1, 10, true);
+    // // Construct like this instead if you want the sampler to not print anything:
+    // // HeatSampler heatSampler(&shutdownManager, 1, 10);
 
-    heatSampler.waitForShutdown();
+    // heatSampler.waitForShutdown();
+
+    // TODO: Remove later. Testing.
+    // Initialize variables.
+    std::string account_sid = twilio::accountSid;
+    std::string auth_token = twilio::authToken;
+    std::string to_number = twilio::toPhoneNumber;
+    std::string from_number = twilio::fromPhoneNumber;
+    std::string message = "hello hello, what have we here?";
+    std::string picture_url = "";
+    bool verbose = true;
+
+    // Instantiate a twilio object and call send_message
+    std::string response;
+    auto twilio = std::make_shared<twilio::Twilio>(
+        account_sid,
+        auth_token
+    );
+
+    bool message_success = twilio->send_message(
+            to_number,
+            from_number,
+            message,
+            response,
+            picture_url,
+            verbose
+    );
+
+    // Report success or failure
+    if (!message_success) {
+            if (verbose) {
+                    std::cout << "Message send failed." << std::endl;
+                    if (!response.empty()) {
+                            std::cout << "Response:" << std::endl
+                                    << response << std::endl;
+                    }
+            }
+            return -1;
+    } else if (verbose) {
+            std::cout << "SMS sent successfully!" << std::endl;
+            std::cout << "Response:" << std::endl << response
+                    << std::endl;
+    }
+
+    message_success = twilio->send_message(
+            to_number,
+            from_number,
+            "2",
+            response,
+            picture_url,
+            verbose
+    );
+
+    // Report success or failure
+    if (!message_success) {
+            if (verbose) {
+                    std::cout << "Message send failed." << std::endl;
+                    if (!response.empty()) {
+                            std::cout << "Response:" << std::endl
+                                    << response << std::endl;
+                    }
+            }
+            return -1;
+    } else if (verbose) {
+            std::cout << "SMS sent successfully!" << std::endl;
+            std::cout << "Response:" << std::endl << response
+                    << std::endl;
+    }
+    message_success = twilio->send_message(
+            to_number,
+            from_number,
+            "3",
+            response,
+            picture_url,
+            verbose
+    );
+
+    // Report success or failure
+    if (!message_success) {
+            if (verbose) {
+                    std::cout << "Message send failed." << std::endl;
+                    if (!response.empty()) {
+                            std::cout << "Response:" << std::endl
+                                    << response << std::endl;
+                    }
+            }
+            return -1;
+    } else if (verbose) {
+            std::cout << "SMS sent successfully!" << std::endl;
+            std::cout << "Response:" << std::endl << response
+                    << std::endl;
+    }
 
     return 0;
 }
