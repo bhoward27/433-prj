d5ef8d24f32a56797fb18a17f846c4b87bf53e79
dhaliwalk <41324852+dhaliwalk@users.noreply.github.com>
2023-03-16 19:37:34 -0700

    changed the reading to be from analog input 1

--- target/src/waterLevelSensor.cpp
+++ target/src/waterLevelSensor.cpp
@@ -8,7 +8,7 @@
 #include <pthread.h>
 #include "utils.h"
 
-#define A2D_FILE_VOLTAGE1  "/sys/bus/iio/devices/iio:device0/in_voltage0_raw"
+#define A2D_FILE_VOLTAGE1  "/sys/bus/iio/devices/iio:device0/in_voltage1_raw"
 #define A2D_VOLTAGE_REF_V  1.8
 #define A2D_MAX_READING    4095
 
